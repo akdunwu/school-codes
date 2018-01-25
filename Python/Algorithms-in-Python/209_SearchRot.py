4b9bac073843471a4a3834e2d9915a7637b197df
Akdun <Akdun@users.noreply.github.com>
2018-01-25 12:11:16 -0500

    Codes added

--- Python/Algorithms-in-Python/209_SearchRot.py
+++ Python/Algorithms-in-Python/209_SearchRot.py
@@ -8,7 +8,16 @@ class Solution:
     """
     def search(self, A, target):
         # write your code here
-
+        l, h = 0, len(A) - 1
+        while (l <= h):
+            m = l + ((h - l) >> 1)
+            if A[m] == target:
+                return m
+            elif (A[m] > A[l] and target < A[m] and target >= A[l]) or (A[m] < A[l] and not (target <= A[h] and target > A[m])):
+                h = m - 1
+            else:
+                l = m + 1
+        return -1
 
 if __name__ == "__main__":
     A=[4, 5, 1, 2, 3]
